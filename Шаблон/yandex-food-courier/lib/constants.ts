50ea4468cce9f40998c62b46f011196567914521
ALogvinenko <natasvetasheva@gmail.com>
2026-06-24 00:59:43 +0300

    1

---
+++ Шаблон/yandex-food-courier/lib/constants.ts
@@ -0,0 +1,9 @@
+export const REG_URL =
+  "https://reg.eda.yandex.ru/?advertisement_campaign=forms_for_agents&user_invite_code=569fd35d6ccc4111852a73036a77109f&utm_content=blank"
+
+export const NAV_LINKS = [
+  { href: "#calculator", label: "Калькулятор" },
+  { href: "#formats", label: "Условия" },
+  { href: "#steps", label: "Как начать" },
+  { href: "#cities", label: "Города" },
+] as const
